765d206cc4f23616ae4cfcf4704bce81e27bd385
beeragere <shubhangarb@gmail.com>
2021-09-24 14:01:17 +0530

    not that much changed

--- calci/calc.js
+++ calci/calc.js
@@ -1,34 +1,53 @@
 var display = document.querySelector(".display");
-
 var num = document.querySelectorAll(".num");
 var symbols = document.querySelectorAll(".symbol");
-
 num.forEach((item)=>{
 	item.addEventListener('click', (e)=>{
-		display.innerHTML += e.target.textContent;
+		let keyPress = e.target.textContent;
+
+		if(Number(display.innerHTML) == 0){
+			display.innerHTML = keyPress;
+		}
+
+		else{
+			display.innerHTML += keyPress;
+		}
 	})
 })
 
 symbols.forEach((items) => {
 	items.addEventListener('click', (e)=>{
-		let innerText = e.target.textContent;
-		if(innerText == 'C'){
-			let displayText = display.innerHTML;	
+		let keyPress = e.target.textContent;
+		let displayText = display.innerHTML;	
+		if(keyPress == 'C'){
 			Length = displayText.length;
 			if(Length > 1){
-				console.log(Length);
 				displayText = displayText.substr(0, Length-1);
 				display.innerHTML = displayText;
-				console.log(displayText);
 			}
 
-			if(displayText[0] != 0){
-				display.innerHTML = 0;
+			if(Length < 2){
+				display.innerHTML = '0';
+			}
+		}
+
+		else if(keyPress == "CC"){
+			display.innerHTML = '0';
+		}
+
+		else if(keyPress == '='){
+			let innerText = display.innerHTML;
+			let textLength = innerText.length;
+			if((isNaN(innerText[0])) || (isNaN(innerText[textLength-1]))){
+				console.error("not a number");
+			} 
+			else{
+				console.log("innerHtml is ", display.innerHTML);
 			}
 		}
 
 		else{
-			display.innerHTML = innerText;
+			display.innerHTML += keyPress;
 		}
 	})
 })
